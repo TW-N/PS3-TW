cb875c3b0cff7afec0ab6937ce2f3725b6738dce
🇸🇦 طلال العنزي <117160019+TW-N@users.noreply.github.com>
2025-02-25 10:30:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.getElementById('HEN1').addEventListener('click', function() {
-  window.location.href = '\HEN1\index.html';
-});
-
-document.getElementById('HEN2').addEventListener('click', function() {
-  window.location.href = '\HEN2\index.html';
-});
-
-
-document.getElementById('HEN3').addEventListener('click', function() {
-  window.location.href = '\HEN3\index.html';
+document.getElementById('HENSA').addEventListener('click', function() {
+  window.location.href = '\HENSA\index.html';
 });
